a204bb76df2f3b8122baaa789d3fd098c50e90a3
ahmedmousa1997 <memo.moza10@yahoo.com>
2019-10-03 17:13:52 +0200

    added login activity nd start and splash

--- app/src/main/java/com/snonosystems/sakeny/LoginActivity.java
+++ app/src/main/java/com/snonosystems/sakeny/LoginActivity.java
@@ -2,15 +2,39 @@ package com.snonosystems.sakeny;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView txtSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtSignup = findViewById(R.id.txt_signup);
+
+
+
+
+
+
+
+
+
+        txtSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(),StartActivity.class));
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/snonosystems/sakeny/StartActivity.java
+++ app/src/main/java/com/snonosystems/sakeny/StartActivity.java
@@ -1,14 +1,22 @@
 package com.snonosystems.sakeny;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class StartActivity extends AppCompatActivity {
 
+    CardView sellerCard , studentCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        sellerCard = findViewById(R.id.card_seller);
+        studentCard = findViewById(R.id.card_buyer);
+
+
     }
 }
