01e34b3d2fad35ac9ab550a7153ec8a9741820db
ahmedmousa1997 <memo.moza10@yahoo.com>
2019-10-03 13:59:15 +0200

    added login activity nd start and splash

---
+++ app/src/main/java/com/snonosystems/sakeny/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.snonosystems.sakeny;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+
+            }
+        },1000);
+
+    }
+}
