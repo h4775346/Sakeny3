b6087e04adf6a2bd386362ac8d40673433201db9
ahmedmousa1997 <memo.moza10@yahoo.com>
2019-10-03 14:05:31 +0200

    added login activity nd start and splash

--- app/src/main/java/com/snonosystems/sakeny/LoginActivity.java
+++ app/src/main/java/com/snonosystems/sakeny/LoginActivity.java
@@ -3,6 +3,7 @@ package com.snonosystems.sakeny;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toast.makeText(this, "asd", Toast.LENGTH_SHORT).show();
     }
+
 }
