e589468926fb6b4f54341192f4d69993f2250e86
h4775346 <englishh7366@gmail.com>
2019-10-03 14:21:45 +0200

    change something

--- app/src/main/java/com/snonosystems/sakeny/LoginActivity.java
+++ app/src/main/java/com/snonosystems/sakeny/LoginActivity.java
@@ -11,8 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toast.makeText(this, "asd", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "asd", Toast.LENGTH_SHORT).show();
+
 
     }
 
