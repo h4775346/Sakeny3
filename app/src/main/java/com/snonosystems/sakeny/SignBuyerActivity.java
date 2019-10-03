080cec95f29aa2744e5e8427f6c77555b293d265
h4775346 <englishh7366@gmail.com>
2019-10-03 17:38:29 +0200

    Added Login Design File

---
+++ app/src/main/java/com/snonosystems/sakeny/SignBuyerActivity.java
@@ -0,0 +1,14 @@
+package com.snonosystems.sakeny;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignBuyerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_buyer);
+    }
+}
